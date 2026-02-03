0c0ad0d33f4d8360142c0773abab36e14a5eb1ca
Johann Patrício Daniel <johannpatriciod@gmail.com>
2026-02-03 18:51:36 -0300

    arrumando bugs

--- backstopConfig.js
+++ backstopConfig.js
@@ -1,58 +1,45 @@
 'use strict';
-// https://github.com/garris/BackstopJS#advanced-scenarios
 
 const backstop = require('@mate-academy/backstop-config');
-const { basicScenario } = backstop;
+
+const baseUrl =
+  'https://JohannPDaniel.github.io/layout_hello-world/moyo-header/';
 
 const basic = {
-  ...basicScenario,
-  label: 'Elementary test',
-  referenceUrl: basicScenario.referenceUrl + '/moyo-header/',
+  url: baseUrl,
+  referenceUrl: baseUrl,
+  delay: 1000,
 };
 
-const config = {
+module.exports = {
   ...backstop,
   fileNameTemplate: '{scenarioLabel}_{viewportLabel}',
-  onBeforeScript: 'puppet/onBefore.js',
-  onReadyScript: 'puppet/onReady.js',
   viewports: [
-    {
-      name: '1024px',
-      width: 1024,
-      height: 768,
-    },
-    {
-      name: '1200px',
-      width: 1200,
-      height: 768,
-    },
+    { name: '1024px', width: 1024, height: 768 },
+    { name: '1200px', width: 1200, height: 768 },
   ],
   scenarios: [
     {
       ...basic,
-      label: 'Header tag',
-      selectors: ['header'],
+      label: 'Header',
+      selectors: ['header'], // 👈 AQUI
     },
     {
       ...basic,
-      label: 'Nav tag',
-      selectors: ['nav'],
+      label: 'Nav',
+      selectors: ['nav'], // 👈 AQUI
     },
     {
       ...basic,
-      misMatchThreshold: 2,
-      label: 'Link with data-qa_hover',
-      selectors: ['[data-qa="hover"]'],
+      label: 'Hover link',
+      selectors: ['[data-qa="hover"]'], // 👈 AQUI
       hoverSelector: '[data-qa="hover"]',
-      postInteractionWait: 1000,
+      postInteractionWait: 500,
     },
     {
       ...basic,
-      misMatchThreshold: 2,
-      label: 'Link with class_is-active',
-      selectors: ['a.is-active'],
+      label: 'Active item',
+      selectors: ['.is-active'], // 👈 AQUI
     },
   ],
 };
-
-module.exports = config;
